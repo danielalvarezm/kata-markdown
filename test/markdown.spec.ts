bd180804f14b77d2e56a6ab49c20370c19bd5d12
Daniel Álvarez Medina <alu0101216126@ull.edu.es>
2022-03-31 11:42:52 +0100

    Test fail: give back header as <h#>

--- test/markdown.spec.ts
+++ test/markdown.spec.ts
@@ -2,7 +2,13 @@ import { MarkDown }  from '../src/markdown';
 
 describe('Markdown should', () => {
     const markDown = new MarkDown();
+    
     it('give back text as <p>' , () => {
         expect(markDown.convert('prueba 1, esto funciona')).toBe('<p>prueba 1, esto funciona</p>');
+    });
+
+    it('give back header as <h#>' , () => {
+        expect(markDown.convert('# prueba h1')).toBe('<h1>prueba h1</h1>');
+        expect(markDown.convert('## prueba h2')).toBe('<h2>prueba h2</h2>');
     });
 });
