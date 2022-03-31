3ffa033b4e4513dc61918225a10e5eb68502bd1f
Daniel Álvarez Medina <alu0101216126@ull.edu.es>
2022-03-31 11:49:00 +0100

    Test success: give back header as <h#>

--- src/markdown.ts
+++ src/markdown.ts
@@ -1,5 +1,11 @@
 export class MarkDown {    
     public convert(content:string): string {
+        const splittedString:string[] = content.split(' ');
+        if(splittedString[0].includes('#')){
+            const size:number = splittedString[0].length;
+            return `<h${size}>`+ splittedString.slice(1).join(' ') + `</h${size}>`;
+        }
+
         return '<p>' + content + '</p>';
     }
 }
